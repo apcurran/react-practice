41239281fbc0ea8ee6c817ebde2954933dd10bf8
apc <alexcurran.va@gmail.com>
2020-03-26 16:07:56 -0500

    Initial setup

---
+++ src/Ninjas.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Component } from "react";
+
+class Ninjas extends Component {
+    render() {
+        return (
+            <div className="ninja">
+                <p>Name: Ryu</p>
+                <p>Age: 30</p>
+                <p>Belt: Black</p>
+            </div>
+        );
+    }
+}
+
+export default Ninjas;
